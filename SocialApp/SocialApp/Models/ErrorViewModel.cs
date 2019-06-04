4f3fddcd1c9d210c65c035782813c7679a58cd55
Антон Смирнов <smirya84@outlook.com>
2019-06-04 22:36:15 +0300

    Create App

---
+++ SocialApp/SocialApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SocialApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
